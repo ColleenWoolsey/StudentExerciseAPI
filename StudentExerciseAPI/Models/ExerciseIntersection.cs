7aca22d81d15ed8ab2351f71d05e39a924b06c17
Colleen Woolsey <rlwool@aol.com>
2019-03-25 15:22:44 -0500

    Chapter2 - Created Exercise controller

---
+++ StudentExerciseAPI/Models/ExerciseIntersection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentExerciseAPI.Models
+{
+    public class ExerciseIntersection
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int ExerciseId { get; set; }
+        public object Exercise { get; internal set; }
+        public object Student { get; internal set; }
+    }
+}
---
+++ StudentExerciseAPI/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using StudentExerciseAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace StudentExercisesAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public string StudentSlackHandle { get; set; }
+        public string CohortName { get; set; }
+        public int CohortId { get; set; }
+        public Cohort Cohort { get; set; }
+        public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+    }
+}
